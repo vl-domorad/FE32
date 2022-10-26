14a7140717a576b64dbb0e338e70f4f22f3f2ee3
Vladislav Domorad <vlad.domorad@mail.ru>
2022-10-26 20:19:02 +0300

    base work with card component

--- src/Assets/icons/index.ts
+++ src/Assets/icons/index.ts
@@ -1,2 +1,6 @@
 export * from "./BurgerClosedIcon";
 export * from "./CloseIcon";
+export { LikeIcon } from './LikeIcon'
+export { DislikeIcon } from './DislikeIcon'
+export { BookmarkIcon } from './BookmarkIcon'
+export { SettingsIcon } from './SettingsIcon'
